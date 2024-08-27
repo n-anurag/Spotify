66293c2b8359d2efe663c26b7b05f695f978c769
n-anurag <anuragneupane58@gmail.com>
2024-08-28 00:08:17 +0545

    spotify frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
 import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
 
 
 const App = () => {
+
+  const {audioRef,track} = useContext(PlayerContext)
+
   return (
     <>
     <div className='h-screen bg-black'>
@@ -14,6 +18,8 @@ const App = () => {
     <Display/>
     </div>
     <Player/>
+    <audio ref={audioRef} src={track.file} preload='auto'></audio>
+   
     </div>
 
     </>
